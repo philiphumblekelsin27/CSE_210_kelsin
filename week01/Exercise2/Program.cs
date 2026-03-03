e6dc18268b47592f56c03d9a77046196a50053c2
philiphumblekelsin27 <humblekelsin271977@gmail.com>
2026-03-03 18:42:06 +0100

    completed the wk 01 exercises spent 10hrs on it. I had to redo the exercises a few times to get them right. I also had to look up some C# syntax and concepts that I was not familiar with. Overall, I learned a lot and I am happy with the progress I made.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,64 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        string sign = "";
+        int lastDigit = percent % 10;
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else if (lastDigit < 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        if (letter == "A" && sign == "+")
+        {
+            sign = "";
+        }
+
+        if (letter == "F")
+        {
+            sign = "";
+        }
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+        if (percent >= 90 || percent >= 80 || percent >= 70 || percent >= 60)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time! Keep studying and you'll get it.");
+        }
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,73 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        List<int> numbers = new List<int>();
+        int userNumber = -1;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            userNumber = int.Parse(Console.ReadLine());
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The largest number is: {max}");
+
+        int smallestPositive = 9999999;
+        int smallestNegative = 0;
+        foreach (int number in numbers)
+        {
+
+            if (number > 0)
+            {
+                if (number < smallestPositive)
+                {
+                    smallestPositive = number;
+                }
+            }
+
+            if (number < 0)
+            {
+                if (number < smallestNegative)
+                {
+                    smallestNegative = number;
+                }
+            }
+        }
+
+        if (smallestPositive != 9999999)
+            Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+        if (smallestNegative != 0)
+            Console.WriteLine($"The smallest negative (most negative) number is: {smallestNegative}");
+        numbers.Sort();
+        Console.WriteLine("The sorted list is:");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
