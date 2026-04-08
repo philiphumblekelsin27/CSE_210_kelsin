3251b7f2b4a2cdb43ad4bfdfc63fa1ee4c6ad124
philiphumblekelsin27 <humblekelsin271977@gmail.com>
2026-04-08 19:44:28 +0100

    completed the sub phase of the project by adding the GoalManager class and starting it in the Main method.

---
+++ week06/EternalQuest/SimpleGoal.cs
@@ -0,0 +1,13 @@
+public class SimpleGoal : Goal
+{
+    private bool _isComplete;
+
+    public SimpleGoal(string name, string description, string points) : base(name, description, points)
+    {
+        _isComplete = false;
+    }
+
+    public override void RecordEvent() { }
+    public override bool IsComplete() { return false; }
+    public override string GetStringRepresentation() { return ""; }
+}
